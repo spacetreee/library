7078c254c1bf795d561d3ec994b51bc73e3880ed
Emma <111027901+spacetreee@users.noreply.github.com>
2023-04-01 10:31:16 -0400

    Add sample object to myLibrary

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const myLibrary = [];
 
+const Test = {
+    title: 'A',
+    author: 'A',
+    pages: 300,
+    read: 'read',
+};
+
+const Test2 = {
+    title: 'A',
+    author: 'A',
+    pages: 300,
+    read: 'read',
+};
+
 // the book constructor
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        console.log(`${title} by ${author}, ${pages}, ${read}`);
+    };
 }
 
+const howlsCastle = new Book(
+    'Howl\'s Moving Castle',
+    'Diana Wynne Jones',
+    336,
+    'read',
+);
+
 function addBookToLibrary() {
-  // do stuff here
+    myLibrary.push(howlsCastle);
+    myLibrary.push(Test);
+    myLibrary.push(Test2);
+}
+
+function displayBookOnPage() {
+    myLibrary.forEach((element) => {
+        console.log(element);
+    });
 }
