912a4d68268b31d453e5ae971ffa93d4fdaed26b
Emma <111027901+spacetreee@users.noreply.github.com>
2023-04-01 09:45:05 -0400

    Create starter files for project

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+// the book constructor
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
